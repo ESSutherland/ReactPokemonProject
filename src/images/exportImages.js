aca750cd1751601eca7a1d0a5a8496446389b09c
Ethan Sutherlannd <ESutherland95@gmail.com>
2017-12-06 03:49:32 -0600

    added new way to get images

---
+++ src/images/exportImages.js
@@ -0,0 +1,5 @@
+export const generation1Images = [
+  {"normal": require('./generation1/normal/bulbasaur.gif'), 'shiny': require('./generation1/shiny/bulbasaur.gif')},
+  {"normal": require('./generation1/normal/ivysaur.gif'), 'shiny': require('./generation1/shiny/ivysaur.gif')},
+  {"normal": require('./generation1/normal/venusaur.gif'), 'shiny': require('./generation1/shiny/venusaur.gif'), 'female': require('./generation1/normal/venusaur-f.gif'), 'female_shiny': require('./generation1/shiny/venusaur-f.gif')}
+]
